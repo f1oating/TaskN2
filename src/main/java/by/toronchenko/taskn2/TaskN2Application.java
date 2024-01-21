5015aa39452ba6e048fed6df7c94a0ee6e759414
f1oating <dtoronchenko@gmail.com>
2024-01-21 22:08:45 +0200

    init commit

---
+++ src/main/java/by/toronchenko/taskn2/TaskN2Application.java
@@ -0,0 +1,13 @@
+package by.toronchenko.taskn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskN2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskN2Application.class, args);
+    }
+
+}
